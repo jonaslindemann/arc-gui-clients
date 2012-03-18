f26e03590b5b21b4173bab60de750d1023476943
jonas_lindemann <jonas_lindemann@32b49ed6-39b5-4a71-ba1d-bd4d897f590f>
2012-03-18 13:21:49 +0000

    Work on Mac OS X packaging.

--- cmake/Modules/FindArc.cmake
+++ cmake/Modules/FindArc.cmake
@@ -9,59 +9,59 @@ include(LibFindMacros)
 
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(Arc_PKGCONF arcbase)
+#libfind_pkg_check_modules(Arc_PKGCONF arcbase)
 
 # Include dir
 find_path(Arc_INCLUDE_DIR
   NAMES arc/UserConfig.h
-  PATHS /usr /Applications/ARC.app/Contents/MacOS/include
+  PATHS /usr $ENV{ARC_LOCATION}/include
 )
 
 # Finally the library itself
 find_library(Arc_CLIENT_LIB
   NAMES arcclient
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Finally the library itself
 find_library(Arc_COMMON_LIB
   NAMES arccommon
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Finally the library itself
 find_library(Arc_DATA2_LIB
   NAMES arcdata2
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Finally the library itself
 find_library(Arc_CREDENTIAL_LIB
   NAMES arccredential
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Finally the library itself
 find_library(Arc_CREDENTIALSTORE_LIB
   NAMES arccredentialstore
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Finally the library itself
 find_library(Arc_CRYPTO_LIB
   NAMES arccrypto
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Finally the library itself
 find_library(Arc_MESSAGE_LIB
   NAMES arcmessage
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 find_library(Arc_LOADER_LIB
   NAMES arcloader
-  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} /Applications/ARC.app/Contents/MacOS/lib
+  PATHS ${Arc_PKGCONF_LIBRARY_DIRS} $ENV{ARC_LOCATION}/lib
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
