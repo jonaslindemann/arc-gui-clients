0bf9a66c369e1056d343b584f5659e0d3037f33d
jonas_lindemann <jonas_lindemann@32b49ed6-39b5-4a71-ba1d-bd4d897f590f>
2012-05-13 12:39:53 +0000

    Adding a arccert-ui.

---
+++ trunk/src/arccert-ui/mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+
+#include "qdebugstream.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    ArcProxyController m_proxyController;
+    QDebugStream* m_debugStream;
+    QDebugStream* m_debugStream2;
+
+    void handleDebugStreamEvent(const DebugStreamEvent *event);
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void customEvent(QEvent * event);
+    
+private Q_SLOTS:
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
