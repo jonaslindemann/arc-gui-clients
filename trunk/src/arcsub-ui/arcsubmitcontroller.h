a67b17dad57dc5a9bc6324b3c082cbcbb5878215
jonas_lindemann <jonas_lindemann@32b49ed6-39b5-4a71-ba1d-bd4d897f590f>
2012-12-16 22:41:07 +0000

    Multithreaded job submit implemented.

---
+++ trunk/src/arcsub-ui/arcsubmitcontroller.h
@@ -0,0 +1,42 @@
+#ifndef __arcsubmitcontroller_h__
+#define __arcsubmitcontroller_h__
+
+#include <list>
+
+#include <QObject>
+#include <QString>
+#include <QFutureWatcher>
+
+#include <arc/client/JobDescription.h>
+
+class ArcSubmitController : public QObject
+{
+    Q_OBJECT
+
+private:
+    QString m_jobListFilename;
+    std::list<Arc::JobDescription> m_jobDescriptions;
+
+    QFutureWatcher<void> m_submissionWatcher;
+
+public:
+    ArcSubmitController();
+    virtual ~ArcSubmitController();
+
+    void setJobListFilename(QString filename);
+
+    void addJobDescription(Arc::JobDescription jobDescription);
+    void clear();
+
+    void startSubmission();
+
+    int submit();
+
+private Q_SLOTS:
+    void submissionFinished();
+
+Q_SIGNALS:
+    void onSubmissionFinished();
+};
+
+#endif
--- trunk/src/arcsub-ui/utils.h
+++ trunk/src/arcsub-ui/utils.h
@@ -12,17 +12,6 @@ std::list<std::string> getSelectedURLsFromUserConfigAndCommandLine(Arc::UserConf
 std::list<std::string> getRejectDiscoveryURLsFromUserConfigAndCommandLine(Arc::UserConfig usercfg, std::list<std::string> rejectdiscovery);
 std::list<std::string> getRejectManagementURLsFromUserConfigAndCommandLine(Arc::UserConfig usercfg, std::list<std::string> rejectmanagement);
 std::list<Arc::Endpoint> getServicesFromUserConfigAndCommandLine(Arc::UserConfig usercfg, std::list<std::string> registries, std::list<std::string> computingelements, std::string requestedSubmissionInterfaceName = "");
-
-class JobSubmitter {
-private:
-    std::string m_jobListFilename;
-public:
-    JobSubmitter();
-    virtual ~JobSubmitter();
-
-    void setJobListFilename(std::string& filename);
-
-    int submit(std::list<Arc::JobDescription>& jobDescriptionList);
-};
+void printjobid(const std::string& jobid, const std::string& jobidfile);
 
 #endif
