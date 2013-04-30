01633f394cb76a500340e481ef0254f7e0f86d5f
jonas_lindemann <jonas_lindemann@32b49ed6-39b5-4a71-ba1d-bd4d897f590f>
2013-04-30 00:36:24 +0000

    NSS Authentication implemented.

--- trunk/src/arcproxy-ui/main.cpp
+++ trunk/src/arcproxy-ui/main.cpp
@@ -1,13 +1,19 @@
 #include "arcproxy-utils.h"
 #include <QApplication>
+#include "arctools.h"
 
 int main(int argc, char *argv[])
 {
+    QApplication app(argc, argv);
+
     QCoreApplication::setOrganizationName("lunarc");
     QCoreApplication::setOrganizationDomain("lu.se");
     QCoreApplication::setApplicationName("arc-proxy-ui");
 
-    ArcProxyController proxyController;
-    proxyController.showProxyUIAppLoop(argc, argv);
+    if (!ARCTools::instance()->initUserConfig(true))
+        return -1;
+
+    //ArcProxyController proxyController;
+    //proxyController.showProxyUIAppLoop(argc, argv);
     return 0;
 }
